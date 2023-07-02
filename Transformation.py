871b7f5fd9db872f5f6a72bba18ecb508d7e6b00
tsannie <tsannie@student.42.fr>
2023-07-03 00:08:01 +0200

    [+] Transformation class

--- Transformation.py
+++ Transformation.py
@@ -1,6 +1,181 @@
 import argparse
-import matplotlib
+import matplotlib as plt
 from plantcv import plantcv as pcv
+import os
+import sys
+
+
+class Transformation:
+    def __init__(self, options):
+        self.options = options
+
+        # original
+        self.img = None
+
+        # gaussian_blur
+        self.blur = None
+
+        # masked
+        self.masked2 = None
+        self.ab = None
+
+        # roi_objects
+        self.roi_objects = None
+        self.hierarchy = None
+        self.kept_mask = None
+
+        # analysis_objects
+        self.mask = None
+        self.obj = None
+
+    def original(self):
+        img, _, _ = pcv.readimage(filename=self.options.image)
+
+        if self.options.debug == "plot":
+            pcv.plot_image(img)
+
+        self.img = img
+        return img
+
+    def gaussian_blur(self):
+        if self.img is None:
+            raise Exception("Need to call original() first")
+
+        s = pcv.rgb2gray_hsv(rgb_img=self.img, channel="s")
+        s_thresh = pcv.threshold.binary(
+            gray_img=s, threshold=60, max_value=255, object_type="light"
+        )
+        s_gblur = pcv.gaussian_blur(img=s_thresh, ksize=(5, 5), sigma_x=0, sigma_y=None)
+
+        if self.options.debug == "plot":
+            pcv.plot_image(s_gblur)
+
+        self.blur = s_gblur
+        return s_gblur
+
+    def masked(self):
+        if self.blur is None:
+            raise Exception("Need to call gaussian_blur() first")
+
+        b = pcv.rgb2gray_lab(rgb_img=self.img, channel="b")
+        b_thresh = pcv.threshold.binary(
+            gray_img=b, threshold=200, max_value=255, object_type="light"
+        )
+        bs = pcv.logical_or(bin_img1=self.blur, bin_img2=b_thresh)
+
+        masked = pcv.apply_mask(img=self.img, mask=bs, mask_color="white")
+
+        masked_a = pcv.rgb2gray_lab(rgb_img=masked, channel="a")
+        masked_b = pcv.rgb2gray_lab(rgb_img=masked, channel="b")
+
+        maskeda_thresh = pcv.threshold.binary(
+            gray_img=masked_a, threshold=115, max_value=255, object_type="dark"
+        )
+        maskeda_thresh1 = pcv.threshold.binary(
+            gray_img=masked_a, threshold=135, max_value=255, object_type="light"
+        )
+        maskedb_thresh = pcv.threshold.binary(
+            gray_img=masked_b, threshold=128, max_value=255, object_type="light"
+        )
+
+        ab1 = pcv.logical_or(bin_img1=maskeda_thresh, bin_img2=maskedb_thresh)
+        ab = pcv.logical_or(bin_img1=maskeda_thresh1, bin_img2=ab1)
+
+        opened_ab = pcv.opening(gray_img=ab)
+
+        xor_img = pcv.logical_xor(bin_img1=maskeda_thresh, bin_img2=maskedb_thresh)
+
+        ab_fill = pcv.fill(bin_img=ab, size=200)
+
+        closed_ab = pcv.closing(gray_img=ab_fill)
+
+        masked2 = pcv.apply_mask(img=masked, mask=ab_fill, mask_color="white")
+
+        if self.options.debug == "plot":
+            pcv.plot_image(masked2)
+
+        self.masked2 = masked2
+        self.ab = ab_fill
+        return masked2
+
+    def roi(self):
+        if self.masked2 is None:
+            raise Exception("Need to call mask() first")
+
+        id_objects, obj_hierarchy = pcv.find_objects(img=self.img, mask=self.ab)
+
+        roi1, roi_hierarchy = pcv.roi.rectangle(img=self.img, x=0, y=0, h=250, w=250)
+
+        if self.options.debug == "plot":
+            pcv.params.debug = "plot"
+
+        roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(
+            img=self.img,
+            roi_contour=roi1,
+            roi_hierarchy=roi_hierarchy,
+            object_contour=id_objects,
+            obj_hierarchy=obj_hierarchy,
+            roi_type="partial",
+        )
+
+        if self.options.debug == "plot":
+            pcv.params.debug = None
+
+        self.roi_objects = roi_objects
+        self.hierarchy = hierarchy3
+        self.kept_mask = kept_mask
+
+        return roi_objects, hierarchy3, kept_mask, obj_area
+
+    def analysis_objects(self):
+        if self.roi_objects is None:
+            raise Exception("Need to call mask() first")
+
+        obj, mask = pcv.object_composition(
+            img=self.img, contours=self.roi_objects, hierarchy=self.hierarchy
+        )
+
+        analysis_image = pcv.analyze_object(
+            img=self.img, obj=obj, mask=mask, label="default"
+        )
+
+        if self.options.debug == "plot":
+            pcv.plot_image(analysis_image)
+
+        self.mask = mask
+        self.obj = obj
+        return analysis_image
+
+    def peudolandmarks(self):
+        if self.mask is None:
+            raise Exception("Need to call analysis_objects() first")
+
+        if self.options.debug == "plot":
+            pcv.params.debug = "plot"
+
+        top_x, bottom_x, center_v_x = pcv.x_axis_pseudolandmarks(
+            img=self.img, obj=self.obj, mask=self.mask, label="default"
+        )
+
+        if self.options.debug == "plot":
+            pcv.params.debug = None
+        return top_x, bottom_x, center_v_x
+
+    def color_histogram(self):
+        if self.mask is None:
+            raise Exception("Need to call analysis_objects() first")
+
+        color_histogram = pcv.analyze_color(
+            rgb_img=self.img,
+            mask=self.kept_mask,
+            colorspaces="all",
+            label="default",
+        )
+
+        if self.options.debug == "plot":
+            pcv.plot_image(color_histogram)
+
+        return color_histogram
 
 
 class options:
@@ -17,116 +192,38 @@ class options:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image", type=str, help="Path to input image file.")
+    parser.add_argument("-src", type=str, help="Path to the source dir.")
+    parser.add_argument("-dst", type=str, help="Path to the destination dir.")
     args = parser.parse_args()
 
-    # Get options
-    options = options(args.image, debug=None)
+    if args.image is not None:
+        options = options(args.image)
+        transformation = Transformation(options)
+        transformation.original()
+        transformation.gaussian_blur()
+        transformation.masked()
+        transformation.roi()
+        transformation.analysis_objects()
+        transformation.peudolandmarks()
+        transformation.color_histogram()
+    else:
+        if args.src is None or args.dst is None:
+            raise Exception("Need to specify src and dst")
 
-    # Set debug to the global parameter
-    pcv.params.debug = options.debug
+        src = args.src
+        dst = args.dst
 
-    # Read image
-    img, path, filename = pcv.readimage(filename=options.image)
+        # TODO SAVE RESULTS
 
-    # Convert RGB to HSV and extract the saturation channel
-    s = pcv.rgb2gray_hsv(rgb_img=img, channel="s")
-
-    # Take a binary threshold to separate plant from background.
-    s_thresh = pcv.threshold.binary(
-        gray_img=s, threshold=70, max_value=255, object_type="light"
-    )
-
-    # Median Blur
-    s_mblur = pcv.median_blur(gray_img=s_thresh, ksize=5)
-
-    # Gaussian blur
-    s_gblur = pcv.gaussian_blur(img=s_mblur, ksize=(5, 5), sigma_x=0, sigma_y=None)
-
-    # RGB -> LAB to extract the Blue channel
-    b = pcv.rgb2gray_lab(rgb_img=img, channel="b")
-
-    # Threshold the blue channel
-    b_thresh = pcv.threshold.binary(
-        gray_img=b, threshold=200, max_value=255, object_type="light"
-    )
-
-    # join the threshold saturation and blue-yellow images
-    bs = pcv.logical_or(bin_img1=s_gblur, bin_img2=b_thresh)
-
-    # Appy Mask
-    masked = pcv.apply_mask(img=img, mask=bs, mask_color="white")
-
-    # Convert RGB to LAB and extract the Green-Magenta and Blue-Yellow channels
-    masked_a = pcv.rgb2gray_lab(rgb_img=masked, channel="a")
-    masked_b = pcv.rgb2gray_lab(rgb_img=masked, channel="b")
-
-    # Threshold the green-magenta and blue images
-    maskeda_thresh = pcv.threshold.binary(
-        gray_img=masked_a, threshold=115, max_value=255, object_type="dark"
-    )
-    maskeda_thresh1 = pcv.threshold.binary(
-        gray_img=masked_a, threshold=135, max_value=255, object_type="light"
-    )
-    maskedb_thresh = pcv.threshold.binary(
-        gray_img=masked_b, threshold=128, max_value=255, object_type="light"
-    )
-
-    # Join the thresholded saturation and blue-yellow images (OR)
-    ab1 = pcv.logical_or(bin_img1=maskeda_thresh, bin_img2=maskedb_thresh)
-    ab = pcv.logical_or(bin_img1=maskeda_thresh1, bin_img2=ab1)
-
-    # Opening filters out bright noise from an image.
-    opened_ab = pcv.opening(gray_img=ab)
-
-    xor_img = pcv.logical_xor(bin_img1=maskeda_thresh, bin_img2=maskedb_thresh)
-
-    # Fill small objects
-    ab_fill = pcv.fill(bin_img=xor_img, size=250)
-
-    # closing filters out dark noise from an image.
-
-    # closed_ab = pcv.closing(gray_img=ab_fill)
-
-    # Apply mask (for vis images, mask_color=white)
-    # masked2 = pcv.apply_mask(img=masked, mask=closed_ab, mask_color="white")
-
-    # Identify objects
-
-    pcv.plot_image(s_gblur)
-
-    pcv.params.debug = "plot"
-    id_objects, obj_hierarchy = pcv.find_objects(img=img, mask=ab)
-
-    # Define ROI
-    roi1, roi_hierarchy = pcv.roi.rectangle(img=img, x=0, y=0, h=250, w=250)
-
-    # Decide which objects to keep
-    roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(
-        img=img,
-        roi_contour=roi1,
-        roi_hierarchy=roi_hierarchy,
-        object_contour=id_objects,
-        obj_hierarchy=obj_hierarchy,
-        roi_type="partial",
-    )
-
-    # Object combine kept objects
-    obj, mask = pcv.object_composition(
-        img=img, contours=roi_objects, hierarchy=hierarchy3
-    )
-
-    ### Analysis ###
-    # Find shape properties, output shape image (optional)
-    analysis_image = pcv.analyze_object(img=img, obj=obj, mask=mask, label="default")
-
-    # Color histogram
-    color_histogram = pcv.analyze_color(
-        rgb_img=img, mask=kept_mask, colorspaces="all", label="default"
-    )
-
-    # Pseudolandmarks
-    top_x, bottom_x, center_v_x = pcv.x_axis_pseudolandmarks(
-        img=img, obj=obj, mask=mask, label="default"
-    )
-
-    exit()
+        for root, dirs, files in os.walk(src):
+            for file in files:
+                if file.endswith(".JPG") or file.endswith(".jpg"):
+                    options = options(os.path.join(root, file), debug=None)
+                    transformation = Transformation(options)
+                    transformation.original()
+                    transformation.gaussian_blur()
+                    transformation.masked()
+                    transformation.roi()
+                    transformation.analysis_objects()
+                    transformation.peudolandmarks()
+                    transformation.color_histogram()
