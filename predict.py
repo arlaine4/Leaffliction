9a1f0c795d0b52fd0ec2ab578560ba4093489ff4
Arthur LAINE <arlaine@paul-f4Br6s3.clusters.42paris.fr>
2023-07-07 12:20:52 +0200

    added a batch mode for prediction, you can run python predict.py -b images/<class>/<sub_class_name> to get your accuracy on an entire class

--- predict.py
+++ predict.py
@@ -9,15 +9,28 @@ from Transformation import Transformation, options
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    """parser.add_argument('-i', default=False,
+                        help='Make prediction from an image path')
+    parser.add_argument('-b', default=False,
+                        help='Make prediction from a directory')
+    parser.add_argument('-m', default='model/model.h5',
+                        action='store_true', help='Model path')
+    parser.add_argument('-c', default='model/class_names.txt',
+                        action='store_true',
+                        help='Path to class names file')"""
     parser.add_argument('-i', '--image', default=False,
                         help='Make prediction from an image path')
     parser.add_argument('-b', '--batch', default=False,
-                        help='Make prediction from a directory')
-    parser.add_argument('-m', '--model', action='store_true',
-                        default='model/model.h5', help='Model path')
-    parser.add_argument('-cn', '--clas_names', action='store_true',
-                        help='Path to class names file')
+                        help='Make prediction from a directory path')
+    parser.add_argument('-m', '--model', default='model/model.h5',
+                        help='Model path')
+    parser.add_argument('-cn', '--class_names', default='model/class_names.csv',
+                        help='class_names.csv path')
+    """parser.add_argument('-cn', '--class_names', action='store_true',
+                        default='model/class_names.csv',
+                        help='class_names.csv path')"""
     options = parser.parse_args()
+    print(options)
     return options
 
 
@@ -27,7 +40,7 @@ def verif_args(args):
     if args.image and args.batch:
         raise Exception('You must choose between batch or image mode, not both')
     if args.image:
-        if not os.path.exist(args.image):
+        if not os.path.exists(args.image):
             raise Exception("Image path is invalid")
         if not args.image.endswith('.JPG'):
             raise Exception("Invalid file extension")
@@ -46,14 +59,58 @@ def make_predictions(pred_path, mode, model, class_names):
         image_resize = cv2.resize(image, (128, 128), interpolation=cv2.INTER_AREA)
 
         y_hat = model.predict(np.expand_dims(image_resize, axis=0))
+        class_name_prediction = class_names[np.argmax(y_hat)]
+        options_img = options(pred_path, debug=None)
+        tr = Transformation(options_img)
+        masked = tr.masked()
+
+        plt.suptitle(f"Prediction: {class_name_prediction}")
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(image_resize)
+        plt.axis("off")
+        plt.subplot(1, 2, 2)
+        plt.imshow(masked)
+        plt.axis("off")
+        plt.show()
+    else:
+        # Batch mode used on images/<class>/<sub_class_name>
+        images = os.listdir(pred_path)
+        y_hat = []
+        valid_classes = []
+        correct_preds = 0
+        for image in images:
+            image_loaded = cv2.imread(os.path.join(pred_path, image))
+            image_resize = cv2.resize(image_loaded, (128, 128), interpolation=cv2.INTER_AREA)
+
+            y_hat = model.predict(np.expand_dims(image_resize, axis=0))
+            class_name_prediction = class_names[np.argmax(y_hat)]
+            true_class = pred_path.split('/')[-1]
+            if class_name_prediction == true_class:
+                correct_preds += 1
+                print(f"Prediction for {image} : \033[32m{class_name_prediction}\033[0m")
+            else:
+                print(f"Prediction for {image} : \033[31m{class_name_prediction}\033[0m")
+        print(f"Got {int((correct_preds / len(images)) * 100)}% valid prediction out of {len(images)} images")
+            #valid_classes.append(class_name_prediction) if true_class == class_name_prediction \
+            #            else valid_classes
+            
+
+
 
 
 if __name__ == "__main__":
     args = parse_args()
     prediction_path, mode = verif_args(args)
 
-    model = load_model(args.model)
-    class_names = np.loadtxt(args.clas_names, dtype=str, delimiter=',')
+    try:
+        model = load_model(args.model)
+    except FileNotFoundError:
+        sys.exit("Model not found, please run train.py first")
+    try:
+        class_names = np.loadtxt(args.class_names, dtype=str, delimiter=',')
+    except FileNotFoundError:
+        sys.exit("Class names file not found, please run train.py first")
     make_predictions(prediction_path, mode, model, class_names)
 
 """if __name__ == "__main__":
